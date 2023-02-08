a2b43871dbb26aeb45f07a184d43381663a5b71f
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2023-02-07 22:36:54 -0500

    problem2

--- src/main/java/Resources/search.java
+++ src/main/java/Resources/search.java
@@ -4,20 +4,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.DataProvider;
 
 import Selenium.com.supplyhouse.Base;
+import Selenium.com.supplyhouse.readDatafromExcel;
 
 public class search extends Base {
 
-	
 
 	public search(RemoteWebDriver driver) {
 		this.driver = driver;
 	}
 	
-	public search search(String modelNum) {
-		System.out.println(modelNum);
-			driver.findElement(By.id("model-number")).sendKeys(modelNum);
+	
+	@DataProvider
+	public Object[][] readExcel() {
+		
+		Object[][] data = new Object[2][1];
+		data[0][0] = "AT082410C";
+		data[1][0] = "AT082410C1";
+		
+		//System.out.println(data[0]);
+	return data;
+	}
+	
+	@DataProvider(name = "readExcel")
+	public search searchT(String model) {
+			driver.findElement(By.id("model-number")).sendKeys(model);
 		driver.findElement(By.xpath("//*[@id='model-number-input-container']/following-sibling::button")).submit();
 		return this;
 		
--- src/test/java/Selenium/com/supplyhouse/problem2.java
+++ src/test/java/Selenium/com/supplyhouse/problem2.java
@@ -12,11 +12,11 @@ public class problem2 extends Base{
 
 	static String data = "AT082410C";
 	
-	@Test
+	@Test(dataProvider = "readExcel")
 	public void ValidsearchTC() {
 		search s = new search(driver);
-System.out.println(data);
-		s.search(data);
+
+		s.searchT(null);
 		Assert.assertEquals(s.match(), data, "item matching");
 		Assert.assertEquals(s.Nomatch(), "Sorry, we couldn't find any matches for \" "+data+"\"", "item not matching");
 	Assert.assertTrue(s.resultsValidation(), "browse results showed");;
